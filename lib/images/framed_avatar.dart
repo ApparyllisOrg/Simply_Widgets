ee28153b4f027238f5c9b014f66e42011222185a
Amaryllis <celeste@saltypandastudios.com>
2023-05-23 22:20:15 +0200

    Add color

--- lib/images/framed_avatar.dart
+++ lib/images/framed_avatar.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:simply_widgets/images/cachedImage.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class FramedAvatar extends StatelessWidget {
-  const FramedAvatar({super.key, required this.size, required this.radius, required this.url, required this.cacheManager});
+  const FramedAvatar(
+      {super.key, required this.size, required this.radius, this.frameColor = Colors.transparent, required this.url, required this.cacheManager});
 
   final double size;
   final double radius;
   final String url;
+  final Color frameColor;
   final CacheManager cacheManager;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
         borderRadius: BorderRadius.circular(radius),
-        child: CachedImage(
-          url: url,
-          cacheKey: url,
-          width: size,
-          height: size,
-          cacheManager: cacheManager,
-        ));
+        child: DecoratedBox(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(0), color: frameColor),
+            child: CachedImage(
+              url: url,
+              cacheKey: url,
+              width: size,
+              height: size,
+              cacheManager: cacheManager,
+            ).padding(all: 3)));
   }
 }
