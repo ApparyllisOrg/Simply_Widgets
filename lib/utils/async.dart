d229befb1e4a3d96d1126b4428be2efb2f4ece2c
Amaryllis <celeste@saltypandastudios.com>
2024-05-31 12:26:06 +0200

    update waitForValue

--- lib/utils/async.dart
+++ lib/utils/async.dart
@@ -1,12 +1,15 @@
-
-Future<bool> waitForValue(bool Function() test, {Duration frequency = const Duration(milliseconds: 300)}) async
-{
-  if (test())
-  {
+Future<bool> waitForValue(bool Function() test,
+    {Duration frequency = const Duration(milliseconds: 300),
+    void Function()? failOp}) async {
+  if (test()) {
     return true;
+  }
+
+  if (failOp != null) {
+    failOp();
   }
 
   await Future.delayed(frequency);
 
-  return waitForValue(test, frequency: frequency);  
+  return waitForValue(test, frequency: frequency);
 }
